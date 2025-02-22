4e67c43da0ce61c53eb0a97f5f419e7a127f837e
Kyaa <kyaa@pishii.com>
2025-02-22 18:41:39 +0100

    added navigation in fullscreen image mode, added transitions

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,46 +1,47 @@
-import React, { useState } from "react";
+import React, { useEffect } from "react";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import "./Carousel.scss";
 
-export default function Carousel({ images, onImageClick }) {
-  const [currentIndex, setCurrentIndex] = useState(0);
+export default function Carousel({ images, currentIndex, setCurrentIndex, onImageClick }) {
 
-  const goPrevious = () => {
-    setCurrentIndex((prevIndex) =>
-      prevIndex === 0 ? images.length - 1 : prevIndex - 1
-    );
-  };
+   useEffect(() => {
+   }, [currentIndex]);
 
-  const goNext = () => {
-    setCurrentIndex((prevIndex) =>
-      prevIndex === images.length - 1 ? 0 : prevIndex + 1
-    );
-  };
+   const goPrevious = () => {
+      setCurrentIndex((prevIndex) =>
+         prevIndex === 0 ? images.length - 1 : prevIndex - 1
+      );
+   };
 
-  if (images.length === 0) {
-    return <div>No images available.</div>;
-  }
+   const goNext = () => {
+      setCurrentIndex((prevIndex) =>
+         prevIndex === images.length - 1 ? 0 : prevIndex + 1
+      );
+   };
 
-  return (
-    <div className="pic-container">
-      <i onClick={goPrevious} className="fa-solid fa-angle-left" />
-      <i onClick={goNext} className="fa-solid fa-angle-right" />
+   if (images.length === 0) {
+      return <div>No images available.</div>;
+   }
 
-      <LazyLoadImage
-        src={images[currentIndex]}
-        alt=""
-        effect="blur"
-        width="100%"
-        height="auto"
-        className="pic"
-        onClick={() => {
-          console.log("🖼️ Image Clicked:", images[currentIndex]);
-          onImageClick(images[currentIndex]); // Send the image URL to FullscreenContext
-        }}
-      />
-      <span className="pic-count">
-        {currentIndex + 1}/{images.length}
-      </span>
-    </div>
-  );
+   return (
+      <div className="pic-container">
+         <i onClick={goPrevious} className="fa-solid fa-angle-left" />
+         <i onClick={goNext} className="fa-solid fa-angle-right" />
+
+         <LazyLoadImage
+            src={images[currentIndex]}
+            alt=""
+            effect="blur"
+            width="100%"
+            height="auto"
+            className="pic"
+            onClick={() => {
+               onImageClick(images[currentIndex]);
+            }}
+         />
+         <span className="pic-count">
+            {currentIndex + 1}/{images.length}
+         </span>
+      </div>
+   );
 }
--- src/pages/Annonce/Annonce.js
+++ src/pages/Annonce/Annonce.js
@@ -4,13 +4,13 @@ import Tag from "../../components/Tag/Tag";
 import Dropdown from "../../components/Dropdown/Dropdown";
 import Carousel from "../../components/Carousel/Carousel";
 import logo from "../../assets/logo.png";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import "./Annonce.scss";
 import { DarkModeContext } from "../../context/DarkModeContext/DarkModeContext";
 import { useFullscreen } from "../../context/FullscreenContext/FullscreenContext";
 import Spinner from "../../components/Spinner/Spinner";
 import { Helmet } from "react-helmet";
-import { FaTimes } from "react-icons/fa"; // Import close icon
+import { FaTimes, FaChevronLeft, FaChevronRight } from "react-icons/fa";
 
 export default function Annonce() {
    const navigate = useNavigate();
@@ -18,6 +18,7 @@ export default function Annonce() {
    const [jsonData, setJsonData] = useState(null);
    const { darkMode } = useContext(DarkModeContext);
    const { fullscreenImage, setFullscreenImage } = useFullscreen();
+   const [currentIndex, setCurrentIndex] = useState(0);
 
    useEffect(() => {
       const fetchData = async () => {
@@ -46,79 +47,115 @@ export default function Annonce() {
    let annonce = jsonData.find((a) => a.id === annonceID);
    if (!annonce) {
       navigate("/404");
-   } else {
-      const firstName = annonce.host.name.split(" ")[0];
-      const lastName = annonce.host.name.split(" ")[1];
-      document.title = annonce.title + " - Kasa";
-      return (
-         <>
-            <Helmet>
-               <title>{annonce.title} - Kasa</title>
-               <meta name="description" content={annonce.description} />
-               <meta name="keywords" content="annonces immobilières, appartement, maison, location, achat" />
-               <meta name="author" content="Rémi Phyu" />
-               <meta property="og:title" content={annonce.title} />
-               <meta property="og:description" content={annonce.description} />
-               <meta property="og:image" content={annonce.pictures[0]} />
-               <meta property="og:url" content={window.location.href} />
-               <meta property="og:type" content="website" />
-               <meta name="twitter:card" content="summary_large_image" />
-               <meta name="twitter:title" content={annonce.title} />
-               <meta name="twitter:description" content={annonce.description} />
-               <meta name="twitter:image" content={annonce.pictures[0]} />
-               <html lang="fr" />
-            </Helmet>
+      return null;
+   }
 
-            {!fullscreenImage && (
-               <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 0.5 }}>
-                  <div className="annonce-pic-container">
-                     <Carousel images={annonce.pictures} onImageClick={setFullscreenImage} />
+   const goPrevious = () => {
+      setCurrentIndex((prevIndex) =>
+         prevIndex === 0 ? annonce.pictures.length - 1 : prevIndex - 1
+      );
+      setFullscreenImage(annonce.pictures[currentIndex === 0 ? annonce.pictures.length - 1 : currentIndex - 1]);
+   };
+
+   const goNext = () => {
+      setCurrentIndex((prevIndex) =>
+         prevIndex === annonce.pictures.length - 1 ? 0 : prevIndex + 1
+      );
+      setFullscreenImage(annonce.pictures[currentIndex === annonce.pictures.length - 1 ? 0 : currentIndex + 1]);
+   };
+
+   const firstName = annonce.host.name.split(" ")[0];
+   const lastName = annonce.host.name.split(" ")[1];
+   document.title = annonce.title + " - Kasa";
+   return (
+      <>
+         <Helmet>
+            <title>{annonce.title} - Kasa</title>
+            <meta name="description" content={annonce.description} />
+            <meta name="keywords" content="annonces immobilières, appartement, maison, location, achat" />
+            <meta name="author" content="Rémi Phyu" />
+            <meta property="og:title" content={annonce.title} />
+            <meta property="og:description" content={annonce.description} />
+            <meta property="og:image" content={annonce.pictures[0]} />
+            <meta property="og:url" content={window.location.href} />
+            <meta property="og:type" content="website" />
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:title" content={annonce.title} />
+            <meta name="twitter:description" content={annonce.description} />
+            <meta name="twitter:image" content={annonce.pictures[0]} />
+            <html lang="fr" />
+         </Helmet>
+
+         {!fullscreenImage && (
+            <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 0.5 }}>
+               <div className="annonce-pic-container">
+                  <Carousel
+                     images={annonce.pictures}
+                     currentIndex={currentIndex}
+                     setCurrentIndex={setCurrentIndex}
+                     onImageClick={(img) => {
+                        setFullscreenImage(img);
+                        setCurrentIndex(annonce.pictures.indexOf(img));
+                     }} />
+               </div>
+
+               <div className="annonce-info-div">
+                  <div className="annonce-info">
+                     <div className="annonce-title">{annonce.title}</div>
+                     <div className="annonce-location">{annonce.location}</div>
+                     <div className="tag-div">
+                        {annonce.tags.map((tag, index) => (
+                           <Tag name={tag} key={`${index}-${tag}`} />
+                        ))}
+                     </div>
                   </div>
 
-                  <div className="annonce-info-div">
-                     <div className="annonce-info">
-                        <div className="annonce-title">{annonce.title}</div>
-                        <div className="annonce-location">{annonce.location}</div>
-                        <div className="tag-div">
-                           {annonce.tags.map((tag, index) => (
-                              <Tag name={tag} key={`${index}-${tag}`} />
-                           ))}
+                  <div className="annonce-info-rating rating-div">
+                     <div className="annonce-info-profile">
+                        <div className="name">
+                           <div>{firstName}</div>
+                           <div>{lastName}</div>
+                        </div>
+                        <div className="host-img">
+                           <img src={annonce.host.picture} alt="Hôte" />
                         </div>
                      </div>
-
-                     <div className="annonce-info-rating rating-div">
-                        <div className="annonce-info-profile">
-                           <div className="name">
-                              <div>{firstName}</div>
-                              <div>{lastName}</div>
-                           </div>
-                           <div className="host-img">
-                              <img src={annonce.host.picture} alt="Hôte" />
-                           </div>
-                        </div>
-                        <div className="annonce-rating">
-                           {Array.from({ length: 5 }).map((_, index) => (
-                              <i key={index} className={`fa-solid fa-star  ${index < annonce.rating ? "colored" : "gray"}`}></i>
-                           ))}
-                        </div>
+                     <div className="annonce-rating">
+                        {Array.from({ length: 5 }).map((_, index) => (
+                           <i key={index} className={`fa-solid fa-star  ${index < annonce.rating ? "colored" : "gray"}`}></i>
+                        ))}
                      </div>
                   </div>
+               </div>
 
-                  <div className="annonce-body">
-                     <Dropdown title="Description" desc={annonce.description} />
-                     <Dropdown title="Équipements" desc={annonce.equipments} />
-                  </div>
+               <div className="annonce-body">
+                  <Dropdown title="Description" desc={annonce.description} />
+                  <Dropdown title="Équipements" desc={annonce.equipments} />
+               </div>
+            </motion.div>
+         )}
+         <AnimatePresence>
+            {fullscreenImage && (
+               <motion.div className="fullscreen-overlay" initial={{ opacity: 0, scale: 0.8 }}
+                  animate={{ opacity: 1, scale: 1 }}
+                  exit={{ opacity: 0, scale: 0.8 }}
+                  transition={{ duration: 0.3, ease: "easeInOut" }}>
+                  <motion.img src={fullscreenImage} alt="Annonce en plein écran" className="fullscreen-image" initial={{ opacity: 0, scale: 0.9 }}
+                     animate={{ opacity: 1, scale: 1 }}
+                     exit={{ opacity: 0, scale: 0.9 }}
+                     transition={{ duration: 0.3 }} />
+                  <motion.button className="arrow-btn left" onClick={goPrevious} whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.9 }}>
+                     <FaChevronLeft size={40} />
+                  </motion.button>
+                  <motion.button className="arrow-btn right" onClick={goNext} whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.9 }}>
+                     <FaChevronRight size={40} />
+                  </motion.button>
+                  <motion.button className="close-btn" onClick={() => setFullscreenImage(null)} whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.9 }}>
+                     <FaTimes size={30} />
+                  </motion.button>
                </motion.div>
             )}
-            {fullscreenImage && (
-               <div className="fullscreen-overlay">
-                  <img src={fullscreenImage} alt="Annonce en plein écran" className="fullscreen-image" />
-                  <button className="close-btn" onClick={() => setFullscreenImage(null)}>
-                     <FaTimes size={30} />
-                  </button>
-               </div>
-            )}
-         </>
-      );
-   }
+         </AnimatePresence>
+      </>
+   );
 }
